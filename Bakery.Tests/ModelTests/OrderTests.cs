0db63594b99cb7e74a2e6ade85b993bd2ea56c95
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-14 10:09:26 -0700

    Add passing Get Order price test

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -11,24 +11,32 @@ namespace Bakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfClassItem_Order()
     {
-      Order newOrder = new Order("test", "test");
+      Order newOrder = new Order("test", "test", 0);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
     public void GetTitle_ReturnTitle_String()
     {
       string title = "Weekly Croissants";
-      Order newOrder = new Order(title, "test");
+      Order newOrder = new Order(title, "test", 800);
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
     }
     [TestMethod]
     public void GetDescription_ReturnDescription_String()
     {
-      string description = "500 Croissants to be delivered every Tuesday at 0900";
-      Order newOrder = new Order("Weekly Croissants", description);
+      string description = "500 chocolate croissants to be delivered every Tuesday at 0900";
+      Order newOrder = new Order("Weekly Croissants", description, 800);
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void GetPrice_ReturnPrice_Int()
+    {
+      int price = 800;
+      Order newOrder = new Order("Weekly Croissants", "500 chocolate croissants to be delivered every Tuesday at 0900", 800);
+      int result = newOrder.Price;
+      Assert.AreEqual(price, result);
     }
   }
 }
