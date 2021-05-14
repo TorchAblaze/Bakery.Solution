2bb5c136b32453ca8cbb6613e97fa809b9cc3108
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-14 12:05:33 -0700

    Add passing test for Get Vendor name

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -11,8 +11,16 @@ namespace Bakery.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceofVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Suzie's Cafe";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
     }
   }
 }
--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -4,5 +4,11 @@ namespace Bakery.Models
 {
   public class Vendor
   {
+    public string Name { get; set; }
+
+    public Vendor(string name)
+    {
+      Name = name;
+    }
   }
 }
