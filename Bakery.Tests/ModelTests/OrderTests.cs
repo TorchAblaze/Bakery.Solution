447df6ccdbd6774fbe42f054d6fc7dc9dc8d3967
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-14 09:55:16 -0700

    Add passing Get Order description test

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -11,18 +11,24 @@ namespace Bakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfClassItem_Order()
     {
-      Order newOrder = new Order("test");
+      Order newOrder = new Order("test", "test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
     public void GetTitle_ReturnTitle_String()
     {
       string title = "Weekly Croissants";
-      Order newOrder = new Order(title);
-
+      Order newOrder = new Order(title, "test");
       string result = newOrder.Title;
-
       Assert.AreEqual(title, result);
+    }
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      string description = "500 Croissants to be delivered every Tuesday at 0900";
+      Order newOrder = new Order("Weekly Croissants", description);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
