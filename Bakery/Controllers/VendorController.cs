87bf5450a88d3c4f0a36d9feaa4b51a25950bb3d
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-14 15:34:05 -0700

    Fix:/Update: vendor controller and cshtml

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -24,5 +24,27 @@ namespace Bakery.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
     }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> vendorAndOrders = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      vendorAndOrders.Add("Vendor", selectedVendor);
+      vendorAndOrders.Add("Orders", vendorOrders);
+      return View(vendorAndOrders);
+    }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> vendorAndOrders = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      vendorAndOrders.Add("Orders", vendorOrders);
+      vendorAndOrders.Add("Vendor", foundVendor);
+      return View("Show", vendorAndOrders);
+    }
   }
 }
